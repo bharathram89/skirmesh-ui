a86a20a569fea9b0bdd14d3d3bebb8726a087872
Bharadwaj Ramachandran <zbram@Bharadwajs-MacBook-Pro.local>
2021-03-21 09:03:05 +0530

    rename index.js file

--- skirmesh-api/index.js
+++ skirmesh-api/index.js
@@ -4,19 +4,19 @@ let io = require('socket.io')(http);
 
 io.on('connection', (socket) => {
 
-    // Log whenever a user connects
-    console.log('user connected');
+    // Log whenever a controller connects
+    console.log('controller connected');
 
     // Log whenever a client disconnects from our websocket server
     socket.on('disconnect', function(){
-        console.log('user disconnected');
+        console.log('controller disconnected');
     });
 
     // When we receive a 'message' event from our client, print out
     // the contents of that message and then echo it back to our client
     // using `io.emit()`
     socket.on('message', (message) => {
-        console.log("Message Received: " + message);
+        console.log("Node update message Received: " + message);
         io.emit('message', {type:'new-message', text: message});    
     });
 });
