128e23791d28c3048ff8cc8ffae04b5ef41150a4
Bharadwaj Ramachandran <zbram@Bharadwajs-MacBook-Pro.local>
2021-03-30 19:17:16 +0530

    adding field Info base

--- skirmesh-ui/src/app/non-secure/field-info/field-info.component.ts
+++ skirmesh-ui/src/app/non-secure/field-info/field-info.component.ts
@@ -11,5 +11,49 @@ export class FieldInfoComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  tab3LearnMore(){
+    var dots = document.getElementById("dots-tab3");
+    var moreText = document.getElementById("more-tab3");
+    var btnText = document.getElementById("btn-tab3");
 
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+  tab2LearnMore(){
+    var dots = document.getElementById("dots-tab2");
+    var moreText = document.getElementById("more-tab2");
+    var btnText = document.getElementById("btn-tab2");
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
+  tab1LearnMore(){
+    var dots = document.getElementById("dots-tab1");
+    var moreText = document.getElementById("more-tab1");
+    var btnText = document.getElementById("btn-tab1");
+
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
 }
